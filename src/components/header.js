bf4a995d98210c2da4e83d4040fb1c1aefe9fd84
Will King <contact@wking.dev>
2020-01-29 08:36:48 -0600

    Fixing header

--- src/components/header.js
+++ src/components/header.js
@@ -33,20 +33,20 @@ const MobileMenu = () => (
           </span>
         </MenuButton>
         <MenuList className="fixed inset-0 bg-black text-white z-40 flex flex-col justify-end p-12 sm:p-16 md:p-20 lg:p-24">
-          <MenuLink href="http://mytaxxwiz.com/" className="font-bold text-lg hover:underline p-4 inline-block mr-12">
+          <MenuLink to="/" className="font-bold text-lg hover:underline p-4 inline-block mr-12">
             Start Your Return
           </MenuLink>
-          <MenuLink as={Link} to="/" className="font-bold text-lg hover:underline p-4 inline-block mr-12">
+          <MenuLink as={Link} to="/hiring" className="font-bold text-lg hover:underline p-4 inline-block mr-12">
             We Are Hiring
           </MenuLink>
-          <MenuLink as={Link} to="/" className="font-bold text-lg hover:underline p-4 inline-block mr-12">
+          <MenuLink as={Link} to="/contact" className="font-bold text-lg hover:underline p-4 inline-block mr-12">
             Contact Us
           </MenuLink>
           <MenuItem className="h-5 flex items-center mt-12" onSelect={() => {}}>
-            <a as={Link} className="block px-4 h-full w-auto hover:text-instagram" href="/">
+            <a as={Link} className="block px-4 h-full w-auto hover:text-instagram" href="https://www.instagram.com/taxxwiz/?hl=en">
               <InstagramIcon className="h-full w-auto" />
             </a>
-            <a as={Link} className="block px-4 h-full w-auto hover:text-facebook" href="/">
+            <a as={Link} className="block px-4 h-full w-auto hover:text-facebook" href="https://www.facebook.com/taxxwiz/">
               <FacebookIcon className="h-full w-auto" />
             </a>
           </MenuItem>
@@ -58,10 +58,10 @@ const MobileMenu = () => (
 
 const DesktopMenu = () => (
   <nav className="flex-1 text-right flex items-center justify-end">
-    <a href="http://mytaxxwiz.com/" className="text-black hover:underline p-4 inline-block mr-6 lg:mr-12">
+    <Link to="/" className="text-black hover:underline p-4 inline-block mr-6 lg:mr-12">
       Save More Money
-    </a>
-    <Link to="/" className="link--highlight text-white p-4 inline-block mr-6 lg:mr-12">
+    </Link>
+    <Link to="/hiring" className="link--highlight text-white p-4 inline-block mr-6 lg:mr-12">
       We Are Hiring
     </Link>
     <Link to="/contact" className="h-12 p-4 -mr-4 text-primary hover:text-primary-dark">
@@ -74,16 +74,16 @@ const Header = ({ label }) => {
   const isMobile = useWindowWidth();
   return (
     <header className="flex items-center w-full px-8 md:px-16 pt-2 md:pt-8 pb-4 bg-white">
-      <a
+      <Link
         className="block relative z-50"
-        href="http://mytaxxwiz.com/"
+        to="/"
         data-track
         data-event-category="Navigation"
         data-event-action="Viewed Home"
         data-event-label={`${label} - Logo`}
       >
         <Image image="logo" className="w-24 md:w-40 block" imgStyle={{ width: '100%', height: 'auto' }} />
-      </a>
+      </Link>
       {isMobile ? <MobileMenu /> : <DesktopMenu />}
     </header>
   );
