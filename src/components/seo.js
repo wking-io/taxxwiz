04f72434cdf7df78bd586ba6ac02aee5b69dde94
Will King <contact@wking.dev>
2020-04-12 20:12:44 -0500

    Updating video quality to use less bandwidth

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,94 +1,91 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { StaticQuery, graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
-const SEO = ({ title, description, image, pathname, article }) => (
-  <StaticQuery
-    query={query}
-    render={({
-      site: {
-        siteMetadata: {
-          defaultTitle,
-          titleTemplate,
-          defaultDescription,
-          siteUrl,
-          defaultImage,
-          twitterUsername,
-        },
+const SEO = ({ title, description, image, pathname, article }) => {
+  const {
+    site: {
+      siteMetadata: {
+        defaultTitle,
+        titleTemplate,
+        defaultDescription,
+        siteUrl,
+        defaultImage,
+        twitterUsername,
       },
-    }) => {
-      const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        image: image || defaultImage,
-        url: `${siteUrl}${pathname || "/"}`,
-      }
-
-      return (
-        <>
-          <Helmet title={seo.title} titleTemplate={titleTemplate}>
-            <link rel="canonical" href={seo.url} />
-            <meta name="description" content={seo.description} />
-            <meta name="image" content={siteUrl + seo.image.src} />
-            {seo.url && <meta property="og:url" content={seo.url} />}
-            {article ? (
-              <meta property="og:type" content="article" />
-            ) : (
-              <meta property="og:type" content="website" />
-            )}
-            {seo.title && (
-              <meta
-                property="og:title"
-                content={seo.title}
-              />
-            )}
-            {seo.description && (
-              <meta property="og:description" content={seo.description} />
-            )}
-            <meta property="og:image" content={siteUrl + seo.image.src} />
-            <meta property="og:image:width" content={seo.image.width} />
-            <meta property="og:image:height" content={seo.image.height} />
-            <meta property="og:image:alt" content={seo.image.alt} />
-            <meta name="twitter:card" content="summary" />
-            {twitterUsername && (
-              <meta name="twitter:creator" content={twitterUsername} />
-            )}
-            {seo.title && (
-              <meta
-                name="twitter:title"
-                content={seo.title}
-              />
-            )}
-            {seo.description && (
-              <meta name="twitter:description" content={seo.description} />
-            )}
-            <meta name="twitter:image" content={siteUrl + seo.image.src} />
-            <meta name="twitter:image:alt" content={seo.image.alt} />
-          </Helmet>
-        </>
-      )
-    }}
-  />
-)
-
-export default SEO
-
-const query = graphql`
-  query SEO {
-    site {
-      siteMetadata {
-        defaultTitle: title
-        titleTemplate
-        defaultDescription: description
-        siteUrl
-        defaultImage: image {
-          src
-          alt
-          width
-          height
+    },
+  } = useStaticQuery(
+    graphql`
+      query SEO {
+        site {
+          siteMetadata {
+            defaultTitle: title
+            titleTemplate
+            defaultDescription: description
+            siteUrl
+            defaultImage: image {
+              src
+              alt
+              width
+              height
+            }
+            twitterUsername
+          }
         }
-        twitterUsername
       }
-    }
+    `
+  );
+  
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: image || defaultImage,
+    url: `${siteUrl}${pathname || "/"}`,
   }
-`
+
+  return (
+    <>
+      <Helmet title={seo.title} titleTemplate={titleTemplate}>
+        <link rel="canonical" href={seo.url} />
+        <meta name="description" content={seo.description} />
+        <meta name="image" content={siteUrl + seo.image.src} />
+        {seo.url && <meta property="og:url" content={seo.url} />}
+        {article ? (
+          <meta property="og:type" content="article" />
+        ) : (
+          <meta property="og:type" content="website" />
+        )}
+        {seo.title && (
+          <meta
+            property="og:title"
+            content={seo.title}
+          />
+        )}
+        {seo.description && (
+          <meta property="og:description" content={seo.description} />
+        )}
+        <meta property="og:image" content={siteUrl + seo.image.src} />
+        <meta property="og:image:width" content={seo.image.width} />
+        <meta property="og:image:height" content={seo.image.height} />
+        <meta property="og:image:alt" content={seo.image.alt} />
+        <meta name="twitter:card" content="summary" />
+        {twitterUsername && (
+          <meta name="twitter:creator" content={twitterUsername} />
+        )}
+        {seo.title && (
+          <meta
+            name="twitter:title"
+            content={seo.title}
+          />
+        )}
+        {seo.description && (
+          <meta name="twitter:description" content={seo.description} />
+        )}
+        <meta name="twitter:image" content={siteUrl + seo.image.src} />
+        <meta name="twitter:image:alt" content={seo.image.alt} />
+      </Helmet>
+    </>
+  )
+}
+
+export default SEO;
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -2,5 +2,9 @@ import React from 'react';
 import { Video } from 'cloudinary-react';
 
 export default function VideoPlayer({ publicId, className = '' }) {
-	return <Video cloudName="dzqdvin5s" publicId={publicId} loop="true" autoplay="true" muted="true" />;
+  return (
+    <Video cloudName="dzqdvin5s" publicId={publicId} loop="true" autoplay="true" muted="true">
+      <Transformation quality="auto" />
+    </Video>
+  );
 }
